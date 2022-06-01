e5ccb754892751142527f2a676b258ddb91fa4ea
whyspaceee <mohammad.abdan.syakura@mail.ugm.ac.id>
2022-06-01 09:19:18 +0700

    fixed bugs

--- maps/src/main/java/com/asd/App.java
+++ maps/src/main/java/com/asd/App.java
@@ -6,7 +6,7 @@ import com.asd.logic.*;
 
 public class App {
     public static void main(String[] args) {
-        String apiKey = "AIzaSyDBYOXL_3tBMGBPmXhVFVXoE3Wq-YIBnc8";
+        String apiKey = "api-key here";
         MapsApi api = new MapsApi(apiKey);
         MST mst = new MST(api);
         MainMenu mainMenu = new MainMenu(mst, api);
